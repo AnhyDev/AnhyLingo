9fc9467c2160e044ea9661603b0c8ae9bc7a8d25
BuildTools <unconfigured@null.spigotmc.org>
2023-11-23 06:02:33 +0200

    Permissions

--- src/main/java/ink/anh/lingo/Permissions.java
+++ src/main/java/ink/anh/lingo/Permissions.java
@@ -1,13 +1,13 @@
 package ink.anh.lingo;
 
 public class Permissions {
-    public static final String FILE_LINGO = "itemlingo.file.lingo";
-    public static final String FILE_OTHER = "itemlingo.file.other";
-    public static final String FILE_DELETE = "itemlingo.file.delete";
-    public static final String DIR_VIEW = "itemlingo.file.view";
-    public static final String ITEMS_INFO = "itemlingo.items.info";
-    public static final String RELOAD = "itemlingo.reload";
-    public static final String NBT_SET = "itemlingo.nbt.set";
-    public static final String NBT_LIST = "itemlingo.nbt.list";
-    public static final String NBT_INFO = "itemlingo.nbt.info";
+    public static final String FILE_LINGO = "anhylingo.file.lingo";
+    public static final String FILE_OTHER = "anhylingo.file.other";
+    public static final String FILE_DELETE = "anhylingo.file.delete";
+    public static final String DIR_VIEW = "anhylingo.file.view";
+    public static final String ITEMS_INFO = "anhylingo.items.info";
+    public static final String RELOAD = "anhylingo.reload";
+    public static final String NBT_SET = "anhylingo.nbt.set";
+    public static final String NBT_LIST = "anhylingo.nbt.list";
+    public static final String NBT_INFO = "anhylingo.nbt.info";
 }
